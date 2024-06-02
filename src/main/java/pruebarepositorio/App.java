a498aca0c7d189b5d94ea811ac164791a048e154
Esparpaco <esparpaco@gmail.com>
2024-06-02 12:15:37 +0200

    new app1

--- src/main/java/pruebarepositorio/App.java
+++ src/main/java/pruebarepositorio/App.java
@@ -10,6 +10,7 @@ public final class App {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
 
 
 
